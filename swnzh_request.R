33246dbaced03b289b301b79c9165b66e407c35d
Steve Crawshaw <murrayjarvis@gmail.com>
2025-01-08 10:11:10 +0000

    cef dataset

---
+++ swnzh_request.R
@@ -0,0 +1,29 @@
+# Clean and process community energy fund project data for SWNZH
+# for publishing on the open data portal
+# wrong encoding, truncated file names, commas in number fields, missing lat \ long, duplicated fields, null fields, date formatting incorrect
+
+pacman::p_load(tidyverse, janitor, sf, glue)
+
+raw_sf <- st_read("data/mattswnzh/CEProjects.shp") |> 
+  clean_names()
+
+raw_sf |> glimpse()
+
+clean_cef_projects_ods_tbl  <- raw_sf |> 
+  mutate(across(-geometry, ~str_remove_all(.x, "�"))) |> 
+  mutate(across(c(match_fund, total_ca_1, total_capi, cumulative, cef_gran_1, cef_grant, rcef_stage, project_1, contains("poten")), ~str_remove(.x, ",") |>
+                  as.integer()),
+         lead_conta = NULL,
+         path = NULL,
+         multiple = NA,
+         lat = NULL,
+         long = NULL,
+         lng = NULL,
+         date_compl = as.Date(date_compl, format = "%d/%m/%Y"),
+         most_rec_2 = as.Date(most_rec_2, format = "%d/%m/%Y")) |>
+  mutate(longitude = sf::st_coordinates(geometry)[,1],
+         latitude = sf::st_coordinates(geometry)[,2],
+         geo_point_2d = paste0("{", latitude, ", ", longitude, "}"))
+
+clean_cef_projects_ods_tbl |>
+  st_write("data/clean_cef_projects_ods_tbl.shp")
