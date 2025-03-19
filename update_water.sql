57d93b233d2e012d3baea7d11384a34ebe914a7e
Steve Crawshaw <murrayjarvis@gmail.com>
2025-03-19 16:26:08 +0000

    update surface water sql clean script

--- update_water.sql
+++ update_water.sql
@@ -6,45 +6,6 @@ USE w;
 LOAD HTTPFS;
 LOAD SPATIAL;
 
--- 'https://opendata.westofengland-ca.gov.uk/api/explore/v2.1/catalog/datasets/surface-water-quality-measurements/exports/parquet?lang=en&refine=site_name%3A%22Royate%20Hill%22&facet=facet(name%3D%22site_name%22%2C%20disjunctive%3Dtrue)&timezone=Europe%2FLondon'
--- 'https://opendata.westofengland-ca.gov.uk/api/explore/v2.1/catalog/datasets/surface-water-quality-measurements/exports/parquet?lang=en&timezone=Europe%2FLondon'
--- .mode duckbox
--- SELECT column_name FROM (DESCRIBE
--- (SELECT * FROM read_parquet('https://opendata.westofengland-ca.gov.uk/api/explore/v2.1/catalog/datasets/surface-water-quality-measurements/exports/parquet?lang=en&refine=site_name%3A%22Royate%20Hill%22&facet=facet(name%3D%22site_name%22%2C%20disjunctive%3Dtrue)&timezone=Europe%2FLondon')
--- LIMIT 2));
-
--- objectid
--- site_id
--- date
--- total_coliforms_as_cfu
--- e_coli_as_cfu
--- total_coliforms_as_mpn
--- e_coli_as_mpn
--- presumptive_enterococci_as_cfu
--- faecal_streptococci_as_cfu
--- temperature
--- ph
--- conductivity
--- dissolved_oxygen
--- dissolved_oxygen_pc
--- phosphate_as_phosphorous
--- salinity
--- clostridium_pefingens
--- ammonium_as_nh4
--- turbidity
--- ammonium_as_nitrate
--- nitrite
--- nitrate
--- faecal_coliforms
--- salmonella
--- biological_oxygen_demand
--- chemical_oxygen_demand
--- suspended_solids
--- geo_point_2d
--- site_name
--- river
-
-
 -- the update is usually the whole dataset as an excel file attached to an email
 CREATE OR REPLACE TABLE update_tbl AS
 SELECT
